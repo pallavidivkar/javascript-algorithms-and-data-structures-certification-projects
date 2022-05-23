833c9367b9344776f1ba73e992ed0adffb81d69b
pallavidivkar <50291126+pallavidivkar@users.noreply.github.com>
2022-05-23 11:56:53 +0530

    Create caesars-cipher.js
    caesars-cipher.js implements Caesar cipher/shift cipher. In a shift cipher, each alphabet is replaced by another alphabet shifted by a fixed number of places. This example implements the ROT13 cipher where the values of the letters are shifted by 13 places. So A - N, B - O, C - P and so on. So the code takes ROT13 encoded string as input and returns a decoded string. All uppercase letters will be decoded. Lowercase and non-alphabetic characters will not be decoded and returned as received.

---
+++ caesars-cipher.js
@@ -0,0 +1,16 @@
+function rot13(str) {
+  let cipher = {
+    'N':'A','O':'B','P':'C','Q':'D','R':'E','S':'F',
+    'T':'G','U':'H','V':'I','W':'J','X':'K','Y':'L',
+    'Z':'M','A':'N','B':'O','C':'P','D':'Q','E':'R',
+    'F':'S','G':'T','H':'U','I':'V','J':'W','K':'X',
+    'L':'Y','M':'Z'
+  }
+  function stringcipher(match, string) {
+    return cipher[match];
+  }
+  return str.replace(/[A-Z]/g, stringcipher);
+  
+}
+
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); //returns THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
